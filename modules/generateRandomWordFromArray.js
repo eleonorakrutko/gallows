24d2bdabe750cb56cd0be84ee0c05162575524f1
Web <web@it.step>
2022-10-18 21:06:42 +0300

    fix3 generate random word

--- modules/generateRandomWordFromArray.js
+++ modules/generateRandomWordFromArray.js
@@ -1,6 +1,7 @@
 const arrayOfRandomWord = ['яблоко', 'телефон', 'программист', 'машина', 'работа', 'отдых', 'компьютер', 'смузи', 'валерчик', 'вейп'];
 
 function randomWord (){
-    return arrayOfRandomWord[Math.random() * (arrayOfRandomWord.length -1)];
+    return arrayOfRandomWord[Math.round(Math.random() * (arrayOfRandomWord.length -1))];
 }
+
 export default randomWord;
