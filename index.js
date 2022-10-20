457a247c81bda078f2a527a44a456d954920c5c1
Web <web@it.step>
2022-10-20 19:12:33 +0300

    render emoty blocks

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import startGame from "./modules/startGame.js";
 import restart from "./modules/restartGame.js";
+import { renderEmptyBlocks } from "./modules/renderEmptyBlocks.js";
 const restartButton = document.getElementById('restartButton');
 restartButton.addEventListener('click', restart)
+const input = document.getElementById('input');
+const sendButton = document.getElementById('send')
+import validateEnterValue from "./modules/validateEnterUser.js";
+
+sendButton.addEventListener('click', validateEnterValue(input))
+
+
 const arrOfImgs = startGame();
+
+renderEmptyBlocks();
