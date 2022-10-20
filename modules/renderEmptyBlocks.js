ac3bd1a1dab8849b297993b5b8eb372387a29cb8
Web <web@it.step>
2022-10-20 19:29:20 +0300

    edit render empty blocks

--- modules/renderEmptyBlocks.js
+++ modules/renderEmptyBlocks.js
@@ -1,11 +1,12 @@
 import splitWord from "./splitWord.js";
 
 const wrapperLetters = document.getElementById('wrapperLetters');
-function renderEmptyBlocks(){
+function renderEmptyBlocks(arrBlocks){
     splitWord().forEach(_ => {
         const block = document.createElement('div');
         block.classList = 'block';
         wrapperLetters.append(block);
+        arrBlocks.push(block);
     })
 }
 
